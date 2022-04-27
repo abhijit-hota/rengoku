f1c72c04f2922472a6e0711c4b39665d61592e31
abhijit-hota <abhihota025@gmail.com>
2022-04-27 19:20:26 +0530

    update: add to db code

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.18
 require github.com/atotto/clipboard v0.1.4
 
 require (
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+)
+
+require (
 	golang.design/x/hotkey v0.3.0
 	golang.design/x/mainthread v0.3.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,27 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"io"
 	"log"
 	"net/http"
+	"regexp"
+	"time"
 
 	"github.com/atotto/clipboard"
+	_ "github.com/mattn/go-sqlite3"
 	"golang.design/x/hotkey"
 	"golang.design/x/hotkey/mainthread"
 )
 
-func dataserver() {
-	http.HandleFunc("/",
-		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Hello")
-		})
-
-	log.Println("Starting data server")
-	err := http.ListenAndServe(":8080", nil)
-	log.Fatal(err)
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
-func fn() {
+var db *sql.DB
+
+func GetTitleFromURL(text string) (title string) {
+	res, err := http.Get(text)
+	handleErr(err)
+
+	defer res.Body.Close()
+	resBytes, err := io.ReadAll(res.Body)
+	str := string(resBytes)
+	re := regexp.MustCompile("<title>(.+)</title>")
+
+	title = re.FindStringSubmatch(str)[1]
+	return
+}
+
+func SaveToDB(url string) {
+	GetTitleFromURL(url)
+	title := GetTitleFromURL(url)
+
+	stmt, err := db.Prepare("INSERT INTO LINKS (url, title, created, last_updated) VALUES (?, ?, ?, ?)")
+	handleErr(err)
+	defer stmt.Close()
+
+	now := time.Now().Unix()
+	_, err = stmt.Exec(url, title, now, now)
+	handleErr(err)
+
+	log.Println("Saved URL.")
+}
+
+func InitializeDB() {
+	var err error
+
+	db, err = sql.Open("sqlite3", "./links.db")
+	handleErr(err)
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS links (
+			id INTEGER NOT NULL PRIMARY KEY, 
+			title text, 
+			url text, 
+			created integer, 
+			last_updated integer
+		)
+	`)
+	handleErr(err)
+}
+
+func ListenForHotKey() {
 	hk := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModShift}, hotkey.Key(0x2f))
 	err := hk.Register()
 	if err != nil {
@@ -29,15 +76,16 @@ func fn() {
 	}
 
 	for range hk.Keydown() {
-		text, err := clipboard.ReadAll()
+		url, err := clipboard.ReadAll()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(text)
+		SaveToDB(url)
 	}
 }
-
 func main() {
-	go dataserver()
-	mainthread.Init(fn)
+	InitializeDB()
+	defer db.Close()
+
+	mainthread.Init(ListenForHotKey)
 }
