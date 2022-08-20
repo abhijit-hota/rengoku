4adb2fd04f41e23f130547a4f181081bf52d0926
abhijit-hota <abhihota025@gmail.com>
2022-08-20 19:28:46 +0530

    fix: content type bug in api wrapper

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,17 @@
 const BASE_URL = "http://localhost:8080";
 export default async function api(path: string, method: string = "GET", body = null) {
-  const res = await fetch(BASE_URL + "/api" + path, {
+  const options: RequestInit = {
     method,
     body: body ? (body instanceof FormData ? body : JSON.stringify(body)) : null,
-  });
+  };
+
+  if (!(body instanceof FormData)) {
+    options.headers = {
+      "Content-Type": "application/json",
+    };
+  }
+  
+  const res = await fetch(BASE_URL + "/api" + path, options);
   if (!res.ok) {
     const error = new Error();
     error.cause = (await res.json()).cause;
